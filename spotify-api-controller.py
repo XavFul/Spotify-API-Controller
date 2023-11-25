0593f1bac91545252ae300f42058d4193194c030
XavFul <116307666+XavFul@users.noreply.github.com>
2023-11-25 14:00:41 -0800

    Create spotify-api-controller.py
    Added script to authorize the application by accessing URL and for subsequent API access, refreshing with the refresh token to get a new access token. Implements functionality to set the active device and start or stop playback

---
+++ spotify-api-controller.py
@@ -0,0 +1,76 @@
+import sys
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+# Set up Spotify API credentials
+client_id = 'YOUR_ID_HERE'
+client_secret = 'YOUR_SECRET_HERE'
+redirect_uri = 'http://localhost:8888/callback'
+scope = 'user-read-playback-state user-modify-playback-state'
+
+# Set up SpotifyOAuth
+sp_oauth = SpotifyOAuth(client_id=client_id, client_secret=client_secret, redirect_uri=redirect_uri, scope=scope)
+
+# Function to refresh the access token
+def refresh_token():
+    token_info = sp_oauth.refresh_access_token(token_info['refresh_token'])
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    return sp, token_info
+
+# Check if token info is already saved
+token_info = sp_oauth.get_cached_token()
+
+# Check if the token_info is None or if the 'access_token' key is not present
+if token_info is None or 'access_token' not in token_info:
+    # If no token info is saved or if loading fails, go through the authorization process
+    auth_url = sp_oauth.get_authorize_url()
+    print(f'Please authorize the application by visiting this URL: {auth_url}')
+    response = input('Paste the URL you were redirected to: ')
+    code = sp_oauth.parse_response_code(response)
+    token_info = sp_oauth.get_access_token(code)
+
+    # Save the token info to a file for future use
+    sp_oauth._save_token_info(token_info)
+
+# Check if the token_info is None or if the 'access_token' key is not present
+if token_info is None or 'access_token' not in token_info:
+    print("Failed to obtain the access token.")
+else:
+    # Check if the token is expired
+    if sp_oauth.is_token_expired(token_info):
+        # If expired, refresh the token
+        sp, token_info = refresh_token()
+
+    # Get a list of available devices and authorize access for this session
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    devices = sp.devices()
+
+# Extract device name and command from command line arguments
+    if len(sys.argv) < 3:
+        print("Usage: python3 spotify-api-controller.py DEVICE_NAME <play, pause>")
+        sys.exit(1)
+
+    device_name = sys.argv[1]
+    command = sys.argv[2]
+
+    # Find the device ID for the specified device name
+    device_id = None
+    for device in devices['devices']:
+        if device['name'] == device_name:
+            device_id = device['id']
+            break
+
+    if device_id is None:
+        print(f"Device with name '{device_name}' not found.")
+        sys.exit(1)
+
+    # Perform the specified command (play or pause) on the specified device
+    if command == 'play':
+        sp.transfer_playback(device_id=device_id, force_play=1)
+        print(f"Playback started on device: {device_name}")
+    elif command == 'pause':
+        sp.pause_playback()
+        sp.transfer_playback(device_id=device_id, force_play=0)
+        print(f"Playback paused on device: {device_name}")
+    else:
+        print("Invalid command. Please use 'play' or 'pause'.")
